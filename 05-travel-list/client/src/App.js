ad0ec2503a31a22c32f7ffb1a0f9b75ee176a11a
longhoang <[hoalanh21112002@gmail.com]>
2023-11-11 11:19:55 +0700

    05 Caculating statistics as Derived State

--- 05-travel-list/client/src/App.js
+++ 05-travel-list/client/src/App.js
@@ -207,13 +207,23 @@ function Item({ item, itemsList, setItemsList }) {
 }
 
 function Stats({ itemsList }) {
+  if (!itemsList?.length)
+    return (
+      <div className="stats">
+        <p>Start adding some items to your packing list 🚀</p>
+      </div>
+    );
+
   const packedQuantity = itemsList?.filter((item) => item.packed)?.length;
   const itemsQuantity = itemsList?.length;
+  const percentage = Math.round((packedQuantity / itemsQuantity) * 100);
   return (
     <div className="stats">
       <p>
-        💼 You have {itemsQuantity} items on your list, and you already packed{" "}
-        {packedQuantity} ({Math.round((packedQuantity / itemsQuantity) * 100)}%)
+        {percentage !== 100
+          ? `💼 You have ${itemsQuantity} items on your list, and you already packed 
+          ${packedQuantity} (${percentage}%)`
+          : "🛫 You got everything, ready to go 🛫"}
       </p>
     </div>
   );
