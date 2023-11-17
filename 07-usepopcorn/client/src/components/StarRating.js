ebd5662708034cab86a2e573434775de105f893e
longhoang <[hoalanh21112002@gmail.com]>
2023-11-17 20:11:15 +0700

    07 improving reusability, flexibility with props for StarRating component

--- 07-usepopcorn/client/src/components/StarRating.js
+++ 07-usepopcorn/client/src/components/StarRating.js
@@ -2,8 +2,16 @@ import { useState } from "react";
 import "../startRating.css";
 import { FaRegStar, FaStar } from "react-icons/fa";
 
-export default function StarRating({ numStars = 10 }) {
-  const [selectedStar, setSelectedStar] = useState(null);
+export default function StarRating({
+  numStars = 10,
+  color = "#ffd43b",
+  size = "12",
+  className = "",
+  message = [],
+  defaultRating = 0,
+  onSetRating,
+}) {
+  const [selectedStar, setSelectedStar] = useState(defaultRating);
   const [curStar, setCurStar] = useState(0);
   // const stars = Array.from({ length: 10 });
 
@@ -16,25 +24,47 @@ export default function StarRating({ numStars = 10 }) {
     setCurStar(null);
   }
   return (
-    <div className="stars">
+    <div className={`stars ${className}`}>
       {Array.from({ length: numStars }, (v, i) => (
         <Star
+          color={color}
+          size={size}
           key={i}
           full={(curStar || selectedStar) >= i + 1}
           onMouseLeave={handleMouseLeave}
           onMouseEnter={() => handleMouseEnter(i)}
-          onSelectedStar={() => setSelectedStar(i + 1)}
+          onSelectedStar={() => {
+            setSelectedStar(i + 1);
+            onSetRating(i + 1);
+          }}
         />
       ))}
 
-      <span className="cur-rating">{curStar || selectedStar || ""}</span>
+      <span
+        className="cur-rating"
+        style={{ color, fontSize: `${size / 10}rem` }}
+      >
+        {message.length && message.length === numStars
+          ? message[(curStar || selectedStar) - 1]
+          : curStar || selectedStar || ""}
+      </span>
     </div>
   );
 }
 
-function Star({ full, onMouseEnter, onMouseLeave, onSelectedStar }) {
+// const customStarStyle = {};
+
+function Star({
+  full,
+  onMouseEnter,
+  onMouseLeave,
+  onSelectedStar,
+  color,
+  size,
+}) {
   return (
     <span
+      style={{ color: color, fontSize: `${size / 10}rem` }}
       role="button"
       className="star"
       onMouseEnter={onMouseEnter}
--- 07-usepopcorn/client/src/index.js
+++ 07-usepopcorn/client/src/index.js
@@ -1,14 +1,32 @@
-import React from "react";
+import React, { useState } from "react";
 import ReactDOM from "react-dom/client";
 // import App from "./App";
 import StarRating from "./components/StarRating";
+
+function Test() {
+  const [moviesRating, setMoviesRating] = useState(0);
+  return (
+    <div>
+      <StarRating color="green" onSetRating={setMoviesRating} />
+      <p>This movie rated {moviesRating} starts</p>
+    </div>
+  );
+}
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     {/* <App /> */}
-    <StarRating numStars={10} />
+    <StarRating
+      numStars={5}
+      color="red"
+      size="2.4rem"
+      message={["Terrible", "Bad", "Okay", "Good", "Amazing"]}
+      defaultRating={3}
+    />
+    <StarRating />
     {/* <StarRating numStars={6} />
     <StarRating /> */}
+    <Test />
   </React.StrictMode>
 );
