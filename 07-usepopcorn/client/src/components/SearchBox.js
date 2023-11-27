c8b0a47690472fe0999b500b978a5552a1231933
longhoang <[hoalanh21112002@gmail.com]>
2023-11-27 20:57:34 +0700

    07 use useRef hook to select DOM elements to build minor features

--- 07-usepopcorn/client/src/components/SearchBox.js
+++ 07-usepopcorn/client/src/components/SearchBox.js
@@ -1,4 +1,31 @@
+import { useEffect, useRef } from "react";
+
 export default function SearchBox({ query, onSetQuery }) {
+  // useEffect(() => {
+  //   const search = document.querySelector(".search-box");
+
+  //   search.focus();
+  //   // window.addEventListener("load", function (e) {
+  //   // });
+  // }, [query]);
+  const inputSearch = useRef(null);
+
+  useEffect(() => {
+    // console.log(inputSearch.current);
+    inputSearch.current.focus();
+    const handleKeyEnter = (e) => {
+      console.log(e.key);
+      if (e.key !== "Enter" || inputSearch.current === document.activeElement)
+        return;
+      onSetQuery("");
+      // inputSearch.current.value = "";
+      inputSearch.current.focus();
+    };
+    document.addEventListener("keydown", handleKeyEnter);
+
+    return () => document.removeEventListener("keydown", handleKeyEnter);
+  }, [onSetQuery]);
+
   return (
     <input
       className="search-box"
@@ -6,6 +33,7 @@ export default function SearchBox({ query, onSetQuery }) {
       placeholder="Search movies..."
       value={query}
       onChange={(e) => onSetQuery(e.target.value)}
+      ref={inputSearch}
     />
   );
 }
