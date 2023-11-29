33822457bb7cdf4c1d79f21304f4c92c9022128c
longhoang <[hoalanh21112002@gmail.com]>
2023-11-29 23:18:03 +0700

    09 removing boiler plate code with class fields

--- 09-classy-weather/src/components/Weather.js
+++ 09-classy-weather/src/components/Weather.js
@@ -4,44 +4,45 @@ import Loader from "./Loader";
 
 class Weather extends React.Component {
   controller = new AbortController();
+  state = { query: "", isLoading: false, weather: [], location: {} };
 
-  constructor(props) {
-    super(props);
-    this.state = { query: "", isLoading: false, weather: [], location: {} };
-    this.setQuery = this.setQuery.bind(this);
-    this.setIsLoading = this.setIsLoading.bind(this);
-    this.setWeather = this.setWeather.bind(this);
-    this.setLocation = this.setLocation.bind(this);
-  }
-  setLocation(loc) {
+  // constructor(props) {
+  //   super(props);
+  //   // this.state = { query: "", isLoading: false, weather: [], location: {} };
+  //   this.setQuery = this.setQuery.bind(this);
+  //   this.setIsLoading = this.setIsLoading.bind(this);
+  //   this.setWeather = this.setWeather.bind(this);
+  //   this.setLocation = this.setLocation.bind(this);
+  // }
+  setLocation = (loc) => {
     // this.setState((curState) => {
     //   return { ...curState, country: c };
     // });
     this.setState({ location: loc });
-  }
+  };
 
-  setQuery(q) {
+  setQuery = (q) => {
     // this.setState((curState) => {
     //   return { ...curState, query: q };
     // });
     this.setState({ query: q });
-  }
+  };
 
-  setIsLoading(is) {
+  setIsLoading = (is) => {
     // this.setState((curState) => {
     //   return { ...curState, isLoading: is };
     // });
     this.setState({ isLoading: is });
-  }
+  };
 
-  setWeather(newWeather) {
+  setWeather = (newWeather) => {
     // this.setState((curState) => {
     //   return { ...curState, weather: newWeather };
     // });
     this.setState({ weather: newWeather });
-  }
+  };
 
-  async fetchWeather() {
+  fetchWeather = async () => {
     const options = {
       method: "GET",
       headers: {
@@ -66,7 +67,34 @@ class Weather extends React.Component {
       if (err.name === "AbortError") return;
       alert(err);
     }
-  }
+  };
+
+  // async fetchWeather() {
+  //   const options = {
+  //     method: "GET",
+  //     headers: {
+  //       "X-RapidAPI-Key": "b9b6ebea93msh4b970af5ab1dc5cp13c490jsn5bfea61646e3",
+  //       "X-RapidAPI-Host": "weatherapi-com.p.rapidapi.com",
+  //     },
+  //     signal: this.controller.signal,
+  //   };
+  //   try {
+  //     const res = await fetch(
+  //       `https://weatherapi-com.p.rapidapi.com/forecast.json?q=${this.state.query}&days=6`,
+  //       options
+  //     );
+  //     const data = await res.json();
+  //     this.setWeather(data.forecast?.forecastday);
+  //     this.setLocation({
+  //       city: data.location?.name,
+  //       country: data.location?.country,
+  //     });
+  //   } catch (err) {
+  //     console.log(err);
+  //     if (err.name === "AbortError") return;
+  //     alert(err);
+  //   }
+  // }
 
   async componentDidUpdate(_, prevState) {
     if (prevState.query === this.state.query) return;
