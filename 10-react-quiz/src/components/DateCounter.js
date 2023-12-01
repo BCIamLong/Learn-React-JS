705d73d497b50eeb9ff2efaf56d9fa07db5908cb
longhoang <[hoalanh21112002@gmail.com]>
2023-12-01 23:02:18 +0700

    10 managing related piece of state with reducer hook

--- 10-react-quiz/src/components/DateCounter.js
+++ 10-react-quiz/src/components/DateCounter.js
@@ -1,19 +1,46 @@
-import { useReducer, useState } from "react";
+import { useReducer } from "react";
+
+const initialState = { count: 0, step: 1 };
 
 function reducer(state, action) {
   // console.log(state, action);
   const { type, payload } = action;
-  if (type === "dec") return state - payload;
-  if (type === "inc") return state + payload;
-  if (type === "setCount") return payload;
-  if (type === "reset") return payload;
+  const { count, step } = state;
+
+  // if (type === "dec") return { ...state, count: count - step };
+  // if (type === "inc") return { ...state, count: count + step };
+  // if (type === "setCount") return { ...state, count: payload };
+  // if (type === "setStep") return { ...state, step: payload };
+  // if (type === "reset") return { count: 0, step: 1 };
+
+  switch (type) {
+    case "dec":
+      return { ...state, count: count - step };
+    case "inc":
+      return { ...state, count: count + step };
+    case "setCount":
+      return { ...state, count: payload };
+    case "setStep":
+      return { ...state, step: payload };
+    case "reset":
+      // return { count: 0, step: 1 };
+      return initialState;
+    default:
+      throw new Error("Unknown action");
+  }
+  // console.log(newState);
+
   // return payload;
 }
 
 export default function DateCounter() {
-  const [step, setStep] = useState(1);
+  // const [step, setStep] = useState(1);
   // const [count, setCount] = useState(0);
-  const [count, dispatch] = useReducer(reducer, 0);
+  // const [step, dispatchStep] = useReducer(reducer, 1);
+  // const [count, dispatch] = useReducer(reducer, 0);
+
+  const [state, dispatch] = useReducer(reducer, initialState);
+  const { count, step } = state;
 
   const date = new Date(
     Date.now() + count * 24 * 60 * 60 * 1000
@@ -25,23 +52,32 @@ export default function DateCounter() {
   });
 
   const handleDecrease = () => {
-    dispatch({ type: "dec", payload: step });
+    // dispatch({ type: "dec", payload: step });
     // setCount((count) => count - step);
+    dispatch({ type: "dec" });
   };
 
   const handleIncrease = () => {
     // setCount((count) => count + step);
-    dispatch({ type: "inc", payload: step });
+    // dispatch({ type: "inc", payload: step });
+    dispatch({ type: "inc" });
   };
 
   const handleReset = () => {
-    setStep(1);
+    // setStep(1);
     // setCount(0);
-    dispatch({ type: "reset", payload: 0 });
+    // dispatch({ type: "reset", payload: 0 });
+    // dispatch({ type: "reset", payload: 0 });
+    dispatch({ type: "reset" });
   };
 
   const handleChange = (e) => {
-    dispatch({ type: "setCount", payload: e.target.value });
+    // dispatch({ type: "setCount", payload: +e.target.value });
+    dispatch({ type: "setCount", payload: +e.target.value });
+  };
+
+  const handleSetStep = (e) => {
+    dispatch({ type: "setStep", payload: +e.target.value });
   };
 
   return (
@@ -52,7 +88,7 @@ export default function DateCounter() {
           min={1}
           max={10}
           value={step}
-          onChange={(e) => setStep(+e.target.value)}
+          onChange={handleSetStep}
         />
         <span>{step}</span>
       </div>
