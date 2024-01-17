e1988f8d4b8bbd088db8e1d6a931e4020fa9f8d4
longhoang <[hoalanh21112002@gmail.com]>
2024-01-17 16:58:03 +0700

    15-redux-intro the legacy way of connecting components to redux

--- 15-redux-intro/src/features/accounts/Balance/index.jsx
+++ 15-redux-intro/src/features/accounts/Balance/index.jsx
@@ -1,8 +1,17 @@
-import { useSelector } from "react-redux";
+// import { useSelector } from "react-redux";
+import PropTypes from "prop-types";
 import styles from "./Balance.module.css";
+import { connect } from "react-redux";
 
-function Balance() {
-  const balance = useSelector((store) => store.account.balance);
+Balance.propTypes = {
+  balance: PropTypes.number | PropTypes.string,
+};
+
+// * now instead use useSelector to access to the store we can use the old way to do that which is use the connect function from Redux
+// * so before react hooks introduced this is a way we use to access to Redux store
+
+function Balance({ balance }) {
+  // const balance = useSelector((store) => store.account.balance);
   return (
     <div className={styles.balance}>
       <p>${balance.toFixed(2)}</p>
@@ -10,4 +19,14 @@ function Balance() {
   );
 }
 
-export default Balance;
+// * now we will create the function mapStateToProps and what this does as the name said we will map state from the Redux store to the props of this component so Balance
+function mapStateToProps(state) {
+  return {
+    balance: state.account.balance,
+  };
+}
+
+// * and now we can pass in the mapStateToProps to the connect function
+// * so connect function what it does is basically return a new function and then take this argument Balance to create new component so return a new component which can access to the state from store via props
+
+export default connect(mapStateToProps)(Balance);
