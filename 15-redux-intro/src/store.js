53eaebfedd59d4b3b4c2baff22e302bb7335e759
longhoang <[hoalanh21112002@gmail.com]>
2024-01-19 15:05:24 +0700

    15-redux-intro the redux dev tools

--- 15-redux-intro/src/store.js
+++ 15-redux-intro/src/store.js
@@ -1,5 +1,6 @@
 import { thunk } from "redux-thunk";
 import { createStore, combineReducers, applyMiddleware } from "redux";
+import { composeWithDevTools } from "@redux-devtools/extension";
 import accountReducer from "./features/accounts/accountSlice";
 import customerReducer from "./features/customers/customerSlice";
 
@@ -11,6 +12,11 @@ const rootReducer = combineReducers({
 // * so to all Redux understand the middleware function we need to use the applyMiddleware in the second params of the createStore, and passing the middleware function type which is in this case thunk
 // * thunk is popular when we handle the asynchronous related to HTTP requests, API call... or something like that
 
-const store = createStore(rootReducer, applyMiddleware(thunk));
+// * so to use redux dev tools we need to install @redux-devtools/extension and import the composeWithDevTools function then wrap it with the applyMiddleware()
+// * it's quite strange but this is how it works
+const store = createStore(
+  rootReducer,
+  composeWithDevTools(applyMiddleware(thunk))
+);
 
 export default store;
