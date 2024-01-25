57181af7895a7f8ba3ff7b1756390298223913f6
longhoang <[hoalanh21112002@gmail.com]>
2024-01-25 20:31:21 +0700

    16-fast-react-pizza setting up tailwind css

--- 16-fast-react-pizza/src/ui/Header.jsx
+++ 16-fast-react-pizza/src/ui/Header.jsx
@@ -1,12 +1,14 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 function Header() {
   return (
     <header>
-      <div>
+      {/* *so to make our class names here can have the good order we can install this https://github.com/tailwindlabs/prettier-plugin-tailwindcss package
+       * so this package here will help us sort the order of the class names and help us easy to manage this long class name right */}
+      <div className="bg-slate-400 p-3 pb-4 pt-4 text-center text-4xl">
         <Link to="/">Fast React Pizza Co</Link>
       </div>
-      <h2>John Nas</h2>
+      <h2 className="text-3xl font-bold">John Nas</h2>
     </header>
   );
 }
