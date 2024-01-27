4d3ce3c26d846a2a4af248533e3b2b041e9d51bc
longhoang <[hoalanh21112002@gmail.com]>
2024-01-27 14:18:31 +0700

    16-fast-react-pizza using CSS Grid

--- 16-fast-react-pizza/src/features/cart/CartOverview.jsx
+++ 16-fast-react-pizza/src/features/cart/CartOverview.jsx
@@ -2,7 +2,7 @@ import { Link } from 'react-router-dom';
 
 function CartOverview() {
   return (
-    <div className="flex items-center justify-center gap-x-3 border-b-4 border-stone-800 bg-stone-700 py-3 text-center text-sm uppercase text-stone-200 sm:py-4 sm:text-base">
+    <div className="flex items-center justify-center gap-x-3 self-end border-b-4 border-stone-800 bg-stone-700 py-3 text-center text-sm uppercase text-stone-200 sm:py-4 sm:text-base">
       <p className="space-x-2 font-light text-stone-300 sm:space-x-3">
         <span>23 pizzas</span>
         <span>$23.45</span>
--- 16-fast-react-pizza/src/ui/AppLayout.jsx
+++ 16-fast-react-pizza/src/ui/AppLayout.jsx
@@ -11,12 +11,12 @@ function AppLayout() {
   // * we have layout for all pages with header, CartOverview is fixed and the main content is the only thing will change across when the url change
   // * so we have the layout with Header and CartOverview  and in main content we can have Cart, Menu, Homepage...
   return (
-    <div>
+    <div className="grid h-screen grid-rows-[auto_1fr_auto]">
       {/* we can also put this loader here and then later on we can style it better like the absolute position to display it better */}
       {isLoading && <Loader />}
       <Header />
 
-      <main>
+      <main className="mx-auto h-full w-full max-w-5xl overflow-auto">
         {/* * we use Outlet component to render the element we get from the child route in nested route */}
         <Outlet />
         {/* {isLoading ? <Loader /> : <Outlet />} */}
