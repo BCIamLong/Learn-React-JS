9f7809e0097829dfdaf9a97e3cf56d664e63119d
longhoang <[hoalanh21112002@gmail.com]>
2024-01-27 17:50:53 +0700

    16-fast-react-app styling buttons element states and transitions

--- 16-fast-react-pizza/src/features/cart/Cart.jsx
+++ 16-fast-react-pizza/src/features/cart/Cart.jsx
@@ -1,23 +1,23 @@
-import { Link } from "react-router-dom";
+import { Link } from 'react-router-dom';
 
 const fakeCart = [
   {
     pizzaId: 12,
-    name: "Mediterranean",
+    name: 'Mediterranean',
     quantity: 2,
     unitPrice: 16,
     totalPrice: 32,
   },
   {
     pizzaId: 6,
-    name: "Vegetale",
+    name: 'Vegetale',
     quantity: 1,
     unitPrice: 13,
     totalPrice: 13,
   },
   {
     pizzaId: 11,
-    name: "Spinach and Mushroom",
+    name: 'Spinach and Mushroom',
     quantity: 1,
     unitPrice: 15,
     totalPrice: 15,
@@ -29,13 +29,28 @@ function Cart() {
 
   return (
     <div>
-      <Link to="/menu">&larr; Back to menu</Link>
+      <Link
+        // * https://tailwindcss.com/docs/transition-property
+        // transition-colors, transition-transform...
+        className="transition-all duration-150 hover:text-yellow-500 hover:underline"
+        to="/menu"
+      >
+        &larr; Back to menu
+      </Link>
 
       <h2>Your cart, %NAME%</h2>
 
-      <div>
-        <Link to="/order/new">Order pizzas</Link>
-        <button>Clear cart</button>
+      <div className="flex items-center gap-x-3">
+        {/* * https://tailwindcss.com/docs/ring-width */}
+        <Link
+          className="inline-block rounded-full bg-yellow-400 px-3 py-2 font-semibold text-stone-700 transition-colors duration-200 visited:bg-yellow-400 hover:bg-yellow-300 focus:outline-none focus:ring-2 focus:ring-yellow-300 focus:ring-offset-2 active:bg-yellow-300"
+          to="/order/new"
+        >
+          Order pizzas
+        </Link>
+        <button className="inline-block rounded-full border-2  border-yellow-200 bg-yellow-100 px-3 py-2 transition-colors duration-200 hover:bg-white">
+          Clear cart
+        </button>
       </div>
     </div>
   );
