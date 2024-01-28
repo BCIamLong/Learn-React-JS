b58c1142eadfb73499ace66c9528e1509eb36d6b
longhoang <[hoalanh21112002@gmail.com]>
2024-01-28 17:32:54 +0700

    16-fast-react-pizza absolute positioning z index and more

--- 16-fast-react-pizza/src/ui/Loader.jsx
+++ 16-fast-react-pizza/src/ui/Loader.jsx
@@ -1,5 +1,11 @@
 function Loader() {
-  return <div className="loader">{/* <p>Loading...</p> */}</div>;
+  return (
+    <div className="absolute inset-0 bg-slate-50/10 backdrop-blur-sm">
+      <div className="loader absolute right-2/4 top-2/4 z-50 -translate-y-2/4 translate-x-2/4">
+        {/* <p>Loading...</p> */}
+      </div>
+    </div>
+  );
 }
 
 export default Loader;
