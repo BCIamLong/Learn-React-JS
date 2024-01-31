56653bd56d204f565406620228085cc2c9e56294
longhoang <[hoalanh21112002@gmail.com]>
2024-01-31 17:55:29 +0700

    16-fast-react-pizza deleting cart items

--- 16-fast-react-pizza/src/features/cart/cartSlice.js
+++ 16-fast-react-pizza/src/features/cart/cartSlice.js
@@ -73,3 +73,11 @@ export const getTotalCartPrice = (store) =>
   store.cart.cart.reduce((sum, item) => sum + item.totalPrice, 0);
 
 export const getCart = (store) => store.cart.cart;
+
+// * we can get this current quantity to show the button so delete button or add to cart button so the ideal that when user click add to cart
+// * then the item is added to cart => now we can display the delete button and user now that this item is already in users cart and they can delete this from here if they want
+// * so they can delete in the menu page not need to go to the cart page to delete
+
+// * so because this selector call back function now need a param and that's id right and therefore we need to accept the function with have param id and then return the function which is exactly the selector callback function type right
+export const getCurrentQuantityById = (id) => (store) =>
+  store.cart.cart.find((item) => item.id === id)?.quantity ?? 0;
